3b79da99376d70073d1a5f8b1b520ec540c3f41f
yangqianhua <yqh1988@126.com>
2018-03-15 15:06:58 +0800

    bug修改

--- SSH2Demo/SSH2Demo/ViewController.m
+++ SSH2Demo/SSH2Demo/ViewController.m
@@ -22,17 +22,23 @@
     SSHWrapper* sshWrapper = [[SSHWrapper alloc] init];
     NSError* error;
 
-    [sshWrapper connectToHost:@"0.0.0.0" port:22 user:@"root" password:@"123" error:&error];
+     [sshWrapper connectToHost:@"0.0.0.0" port:22 user:@"root" password:@"1111" error:&error];
 
     if(error){
         NSLog(@"出错");
     }else{
         NSLog(@"未出错");
+        //执行“ls -al”执行
         NSString * result = [sshWrapper executeCommand:@"ls -al" error:nil];
         NSLog(@"---%@",result);
         
-        result = [sshWrapper executeCommand:@"df -h" error:nil];
-        NSLog(@"---%@",result);
+        //执行“df -h”执行
+        NSString* result2 = [sshWrapper executeCommand:@"df -h" error:nil];
+        NSLog(@"---%@",result2);
+        
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"结果" message:[NSString stringWithFormat:@"%@\n\n%@",result,result2] delegate:nil cancelButtonTitle:@"关闭" otherButtonTitles:@"确定", nil];
+        
+        [alert show];
     
     }
 }
